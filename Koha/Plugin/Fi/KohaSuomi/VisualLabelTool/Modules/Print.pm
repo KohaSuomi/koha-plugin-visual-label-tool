40a0a52f9f8c43208c43c9821a1af12ff67087f2
Johanna Raisa <johanna.raisa@gmail.com>
2022-04-05 10:42:36 +0300

    Add location fix to print

--- Koha/Plugin/Fi/KohaSuomi/VisualLabelTool/Modules/Print.pm
+++ Koha/Plugin/Fi/KohaSuomi/VisualLabelTool/Modules/Print.pm
@@ -144,7 +144,8 @@ sub getDescriptionName {
     my $response = $data->{$key}->{$value};
     return Koha::Libraries->find($response)->branchname if $value eq "homebranch";
     return $self->fields->signumYKL($data->{$key}->{'itemcallnumber'}) if $value eq "signumYKL";
-    return $self->fields->signumLoc($data->{$key}->{'itemcallnumber'}) if $value eq "signumLoc";
+    return $self->fields->signumLoc($data->{$key}->{'permanent_location'}, $data->{$key}->{'location'}) if $value eq "signumLoc";
+    return $self->fields->signumHeading($data->{$key}->{'itemcallnumber'}) if $value eq "signumHeading";
     return $self->fields->marcField($data->{$key},$value) if $key eq "marc";
     return $self->fields->customField($data, $key) if !$value;
     return Koha::AuthorisedValues->search({ category => 'MTYPE', authorised_value => $response })->next->lib if $value eq 'itemtype';
