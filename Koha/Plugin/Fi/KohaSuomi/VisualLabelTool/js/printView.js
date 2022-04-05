3206df51566ae764a007754aebad4a671f63f8e4
Johanna Raisa <johanna.raisa@gmail.com>
2022-04-05 14:58:22 +0300

    Remove borders from actual print

--- Koha/Plugin/Fi/KohaSuomi/VisualLabelTool/js/printView.js
+++ Koha/Plugin/Fi/KohaSuomi/VisualLabelTool/js/printView.js
@@ -11,7 +11,7 @@ const printView = Vue.component('print-view', {
                         <div class="print-row" :style="label.signum && label.type != 10 ? \'width:90%;\' : \'width:100%;\'">\
                             <div v-for="label in prints">\
                                 <div :style="label.dimensions">\
-                                    <div style="height: 100%; position:relative; border: 1px solid;">\
+                                    <div style="height: 100%; position:relative;" :style="test ? \'border: 1px solid;\' : \'\'">\
                                         <span v-for="(field, index) in label.fields" :style="field.dimensions" style="position:absolute;">\
                                             <span v-if="field.name == \'items.barcode\'"><barcode :value="field.value" :fontsize="field.dimensions.fontSize"></barcode></span>\
                                             <span v-else>{{field.value}}</span>\
@@ -23,7 +23,7 @@ const printView = Vue.component('print-view', {
                         <div v-if="label.signum" :class="[label.type == 10 ? \'print-row\' : \'\']">\
                             <div v-for="label in prints">\
                                 <div :style="label.signum.dimensions">\
-                                    <div style="height: 100%; position:relative; border: 1px solid; margin-left:2mm">\
+                                    <div style="height: 100%; position:relative; margin-left:2mm" :style="test ? \'border: 1px solid;\' : \'\'">\
                                         <span v-for="(field, index) in label.signum.fields" :style="field.dimensions" style="position:absolute;">\
                                             {{field.value}}\
                                         </span>\
@@ -35,7 +35,7 @@ const printView = Vue.component('print-view', {
                 </div>\
             </div>\
         </div>',
-  props: ['label', 'prints'],
+  props: ['label', 'prints', 'test'],
 });
 
 export default printView;
