e4ccae7769942e553c3e9f86b7bb7c5f709f3f69
Johanna Raisa <johanna.raisa@gmail.com>
2022-04-06 08:48:40 +0300

    Calculate print row width

--- Koha/Plugin/Fi/KohaSuomi/VisualLabelTool/js/printView.js
+++ Koha/Plugin/Fi/KohaSuomi/VisualLabelTool/js/printView.js
@@ -8,7 +8,7 @@ const printView = Vue.component('print-view', {
             <div class="d-flex justify-content-center pb-2">\
                 <div id="printLabel">\
                     <div class="print-row" :class="[label.type == \'roll\' ? \'roll-width\' : \'a4-width\']">\
-                        <div class="print-row" :style="label.signum && label.type != 10 ? \'width:90%;\' : \'width:100%;\'">\
+                        <div class="print-row" :style="labelWidth">\
                             <div v-for="label in prints">\
                                 <div :style="label.dimensions">\
                                     <div style="height: 100%; position:relative;" :style="test ? \'border: 1px solid;\' : \'\'">\
@@ -23,7 +23,7 @@ const printView = Vue.component('print-view', {
                         <div v-if="label.signum" :class="[label.type == 10 ? \'print-row\' : \'\']">\
                             <div v-for="label in prints">\
                                 <div :style="label.signum.dimensions">\
-                                    <div style="height: 100%; position:relative; margin-left:2mm" :style="test ? \'border: 1px solid;\' : \'\'">\
+                                    <div style="height: 100%; position:relative;" :style="test ? \'border: 1px solid;\' : \'\'">\
                                         <span v-for="(field, index) in label.signum.fields" :style="field.dimensions" style="position:absolute;">\
                                             {{field.value}}\
                                         </span>\
@@ -36,6 +36,19 @@ const printView = Vue.component('print-view', {
             </div>\
         </div>',
   props: ['label', 'prints', 'test'],
+  computed: {
+    labelWidth: function () {
+      let newwidth = parseInt(this.label.dimensions.width);
+      let unit = this.label.dimensions.width.substr(
+        newwidth.toString().length - this.label.dimensions.width.length
+      );
+      if (this.label.type != 'roll') {
+        newwidth = newwidth * 2;
+      }
+      newwidth = newwidth + unit;
+      return { width: newwidth };
+    },
+  },
 });
 
 export default printView;
