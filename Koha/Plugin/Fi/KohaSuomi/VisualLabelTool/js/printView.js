465e2109854416ac920d1192d571ee78845be996
Johanna Raisa <johanna.raisa@gmail.com>
2022-05-11 12:34:10 +0300

    Add letterspacing to small text

--- Koha/Plugin/Fi/KohaSuomi/VisualLabelTool/js/printView.js
+++ Koha/Plugin/Fi/KohaSuomi/VisualLabelTool/js/printView.js
@@ -14,7 +14,7 @@ const printView = Vue.component('print-view', {
                                     <div style="height: 100%; position:relative; overflow: hidden" :style="test ? \'border: 1px solid;\' : \'\'">\
                                         <span v-for="(field, index) in label.fields" :style="field.dimensions" style="position:absolute;">\
                                             <span v-if="field.name == \'items.barcode\'"><barcode :value="field.value" :fontsize="field.dimensions.fontSize"></barcode></span>\
-                                            <span v-else>{{field.value}}</span>\
+                                            <span v-else :style="smallText(field.dimensions.fontSize) ? \'letter-spacing: 0.5px;\' : \'\'">{{field.value}}</span>\
                                         </span>\
                                     </div>\
                                 </div>\
@@ -81,6 +81,13 @@ const printView = Vue.component('print-view', {
         } 
       });
       return match;
+    },
+    smallText: function(size) {
+      let spacing = false;
+      if (parseInt(size) <= 10) {
+        spacing = true;
+      }
+      return spacing;
     }
   }
 });
