6117fce67c5f232f160034b7cf972a34338705cb
lmstrand <lmstrand@gmail.com>
2022-06-17 13:12:57 +0300

    Descriptive placeholders for margin fields on print

--- Koha/Plugin/Fi/KohaSuomi/VisualLabelTool/js/margins.js
+++ Koha/Plugin/Fi/KohaSuomi/VisualLabelTool/js/margins.js
@@ -1,20 +1,20 @@
 const margins = Vue.component('margins-component', {
   template:
     '<div class="form-inline">\
-      <div class="form-group mr-2" style="width:120px;">\
+      <div class="form-group mr-2" style="width:210px;">\
           <div class="input-group">\
           <div class="input-group-prepend">\
               <span class="input-group-text"><i class="fas fa-arrow-down" title="Ylämarginaali"></i></span>\
           </div>\
-          <input type="text" class="form-control " name="topMargin" v-model="topMargin" @change="setTopMargin($event)"/>\
+          <input type="text" class="form-control " name="topMargin" placeholder="Ylämarginaali(mm)" v-model="topMargin" @change="setTopMargin($event)"/>\
           </div>\
       </div>\
-      <div class="form-group mr-2" style="width:120px;">\
+      <div class="form-group mr-2" style="width:225px;">\
           <div class="input-group">\
           <div class="input-group-prepend">\
               <span class="input-group-text"><i class="fas fa-arrow-right" title="Vasen marginaali"></i></span>\
           </div>\
-          <input type="text" class="form-control" name="leftMargin" v-model="leftMargin" @change="setLeftMargin($event)"/>\
+          <input type="text" class="form-control" name="leftMargin" placeholder="Vasen marginaali(mm)" v-model="leftMargin" @change="setLeftMargin($event)"/>\
           </div>\
       </div>\
     </div>\
