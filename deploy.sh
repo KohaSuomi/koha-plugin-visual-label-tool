7210e093f4dbc866ea41ee6d9750a0ac75d4b965
Johanna Raisa <johanna.raisa@gmail.com>
2022-09-27 11:07:42 +0300

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PM_FILE="Koha/Plugin/Fi/KohaSuomi/VisualLabelTool.pm"
+VERSION=`grep -oE "\-?[0-9]+\.[0-9]+\.[0-9]" $PM_FILE | head -1`
+MINVERSION=`grep -oE "\-?[0-9]+\.[0-9][0-9]" $PM_FILE | head -1`
+RELEASE_FILE="koha-plugin-visual-label-tool-v${VERSION}.kpz"
+VERSIONTAG=`git tag -l "v${VERSION}-koha-$MINVERSION"`
+
+if [ $VERSIONTAG ]; then
+    echo "Release version already exists!"
+    exit 1
+fi
+
+rm $RELEASE_FILE
+
+echo "Building release package ${RELEASE_FILE}"
+
+zip -r $RELEASE_FILE ./Koha
+
+echo "Creating tag v${VERSION}-koha-$MINVERSION"
+
+git tag -a "v${VERSION}-koha-$MINVERSION" -m "Release ${VERSION}"
